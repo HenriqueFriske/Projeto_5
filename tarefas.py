36339f1f86fc07f7fe62e06c6a63951a8842db33
HenriqueFriske <henriquekulbabafriske@gmail.com>
2025-04-29 22:46:13 -0300

    Refatoração 2: separação da lógica da interface com o usuário

--- tarefas.py
+++ tarefas.py
@@ -1,20 +1,17 @@
 tarefas = []
 
-def criar_tarefa():
-    descricao = input("Descrição: ")
-    status = input("Status (Pendente/Concluído): ")
+
+def criar_tarefa(descricao, status):
     tarefas.append({"descricao": descricao, "status": status})
 
 def listar_tarefas():
     for tarefa in tarefas:
         print("Descrição:", tarefa["descricao"], "- Status:", tarefa["status"])
 
-def concluir_tarefa():
-    descricao = input("Descrição da tarefa a concluir: ")
+def concluir_tarefa(descricao):
     for tarefa in tarefas:
         if tarefa["descricao"] == descricao:
             tarefa["status"] = "Concluído"
-
 while True:
     print("1-Adicionar 2-Listar 3-Concluir 4-Sair")
     opcao = input("Escolha: ")
